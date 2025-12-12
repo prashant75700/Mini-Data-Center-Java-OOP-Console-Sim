ed84954bd7fe4498cd2538c4f528c5e7a5941ae5
Prashant Shukla <67216199+prashant75700@users.noreply.github.com>
2025-12-12 17:26:08 +0530

    Refactor Server class constructor and enhance displayinfo method; add WebServer class with request handling and static file serving

--- DataCenterSimulation.java
+++ DataCenterSimulation.java
@@ -9,7 +9,7 @@ class Server {
     protected int cpuUsagePercet;
     protected int memoryUsesPercent;
 
-    public Server (String hostname, String ipAddress, int cpuCores, int ramGB, int diskGB, boolean isRunning, int cpuUsagePercet, int memoryUsesPercent) {
+    public Server (String hostname, String ipAddress, int cpuCores, int ramGB, int diskGB) {
         this.hostname = hostname;
         this.ipAddress = ipAddress;
         this.cpuCores = cpuCores;
@@ -73,7 +73,65 @@ class Server {
     public void displayinfo() {
         System.out.println("\n===========================");
         System.out.println(" " + hostname);
-        System.out.println("");
+        System.out.println("===========================");
+        System.out.println(" IP: " + ipAddress);
+        System.out.println(" CPU: " + cpuCores + " cores");
+        System.out.println(" RAM: " + ramGB + "GB");
+        System.out.println(" DISK: " + diskGB + "GB");
+        System.out.println(" STATUS: " + (isRunning ? "RUNNING 🟢" : "STOPPED 🛑"));
+        System.out.println("╚════════════════════════════════╝");
     }
+
+}
+// now here we are going to make some classes like WEBSERVER, DATABASE_SERVER, LOAD_BALANCER etc:
+
+class WebServer extends Server { 
+    private int port;
+    private int activeConnections;
+    private String webFramework;
+    
+    public WebServer(String hostname, String ipAddress, int cpuCores, int ramGB, int diskGB,int port) {
+    super(hostname, ipAddress, cpuCores, ramGB, diskGB);
+    this.port = port;
+    this.activeConnections = 0;
+    this.webFramework = "Sora";
+    }
+
+    @Override
+    public void start() {
+        super.start();
+        System.out.println("🌐 Web server listening on port " + port);
+    }
+
+    public void handleRequest (String clientIP) {
+        if (isRunning) {
+            activeConnections++;
+            cpuUsagePercet +=5;
+            memoryUsesPercent +=3;
+            System.out.println(" Handling request from " + clientIP + " (Active: " + activeConnections + ")");
+        }
+        else {
+            System.out.println("Server DOWN - Cannot handle request");
+        }
+    }
+
+    public void serverStaticFile(String filename) { 
+        if (isRunning) {
+            System.out.println("Serving file: " + filename);
+        }
+    }
+
+    @Override
+    public void displayinfo() {
+        super.displayinfo();
+        System.out.println("Type: WEB SERVER 🌐");
+        System.out.println("Port: " + port);
+        System.out.println("Framework: " + webFramework);
+        System.out.println("Active Connections: " + activeConnections);
+    }
+}
+
+//DATABASE SERVER - TO STORE DATA
+class DatabaseServer extends Server {
 
 }
